96e7ac030c22acab56b7fa4762042331692b4712
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-05-23 16:26:21 +0530

    Practicing JEST

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,6 @@
 import {describe, expect, it } from '@jest/globals';
-import { multiply, sum } from './src';
-
+import { app, multiply, sum } from './src';
+import request  from 'supertest';
 
 describe('sum module', () =>{
     it('adds 1 + 2 to equal to 3', ()=>{
@@ -22,5 +22,34 @@ describe ('multiply module', ()=>{
     })
 })
 
+describe("POST /sum", ()=>{
+    it("should return sum of 2 numbers", async () =>{
+        const res = await request(app).post("/sum").send({
+            a:1,
+            b:2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.ans).toBe(3);
+    });
+})
 
 
+describe("GET /sum", () =>{
+    it("Should return sum of 2 numbers ", async () =>{
+        const res =await request(app)
+        .get("/sum")
+        .set({
+            a: "1",
+            b: "2"
+        })
+        .send();
+        expect(res.statusCode).toBe(200);
+        console.log(res.body);
+       expect(res.body.answer).toBe(3);
+    })
+    it("Bad inputs", async () =>{
+        const res = await request(app)
+        .get("/sum").send();
+        expect(res.statusCode).toBe(411);
+    })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,44 @@
+import express  from "express";
+import { z } from "zod";
 export function sum(a:number, b:number){
     return a + b;
 }
 export function multiply(a:number, b:number){
     return a*b; 
 }
+
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+export const app =express();
+
+app.use(express.json());
+app.post("/sum", (req, res) =>{
+    const a = req.body.a;
+    const b = req.body.b;
+    const ans = a + b;
+
+    res.json({
+        ans
+    })
+});
+
+app.get("/sum", (req, res) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        answer
+    })
+});
